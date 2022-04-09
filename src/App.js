0506af7ebd975e8b54c5e0d2b0ca78c403afc784
Sergey <skaper777@gmail.com>
2022-04-09 17:47:09 +0300

    perf: :zap: basic react

--- src/App.js
+++ src/App.js
@@ -3,17 +3,73 @@ import './App.css';
 import Event from './Event/Event'
 
 class App extends Component {
+  state = {
+    events: [
+       {
+          name: 'Run',
+          members: 6
+       },
+       {
+          name: 'Cycle',
+          members: 10
+       }
+    ],
+    pageTitle: "Hello, Let 's Go!",
+    showEvents: false
+  }  
+
+  toggleEventsHandler = () => {
+    this.setState({
+      showEvents: !this.state.showEvents
+    })
+  }
+
+  changeNameHandler(name, index) {
+    const event = this.state.events[index]
+    event.name = name
+
+    const events = [...this.state.events]
+    events[index] = event
+
+    this.setState({events})
+  }
+
+  deleteHandler(index) {
+    let events = [...this.state.events]
+
+    events.splice(index, 1)
+
+    this.setState({events})
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center'
     }
 
+    let events = null 
+
+    if (this.state.showEvents) events = this.state.events.map((event, index) => {
+      return (
+        <Event 
+          key={index}
+          name={event.name} 
+          members={event.members}
+          onChangeName={e => this.changeNameHandler(e.target.value, index)}
+          onDelete={this.deleteHandler.bind(this, index)}
+        />
+      )          
+    })
+
     return ( 
       <div style = {divStyle}>
-        <h1> Hello, Let 's Go!</h1>
-        <Event name='Run' members='6'>
-          <p>Test</p>
-        </Event>
+        <h1>{this.state.pageTitle}</h1>
+
+        <button 
+          onClick={this.toggleEventsHandler}> 
+            Show events!</button>
+
+        { events }    
       </div>
     )
   }
--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,9 +1,15 @@
-function Event({name, members, children}) {
+function Event(props) {
   return (
-    <div>
-      <h2>Event '{name}'</h2>
-      <p>Members: {members}</p>
-      { children }
+    <div style={{
+      border: '1px solid #ccc',
+      marginBottom: '10px',
+      display: 'block',
+      padding: '10px'
+    }}>
+      <h2>Event '{props.name}'</h2>
+      <p>Members: {props.members}</p>     
+      <input type="text" onChange={props.onChangeName} value={props.name} />
+      <button onClick={props.onDelete}>Delete</button>
     </div>
   )        
 }
