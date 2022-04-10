279cdfb05be48953c9caa6f01dbfdaf10fc280ca
Sergey <skaper777@gmail.com>
2022-04-10 16:23:58 +0300

    feat: :zap: style components

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,14 +1,28 @@
+import classes from './Event.module.scss'
+
+console.log(classes)
+
 function Event(props) {
+  const inputClasses = [classes.input]
+
+  if (props.name.length) inputClasses.push(classes['input--green'])
+  else inputClasses.push(classes['input--red'])
+
+  const style = {
+    border: '1px solid #fcc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .94)'
+  }
+
   return (
-    <div style={{
-      border: '1px solid #ccc',
-      marginBottom: '10px',
-      display: 'block',
-      padding: '10px'
-    }}>
+    <div className={classes.Event} style={style}>
       <h2>Event '{props.name}'</h2>
       <p>Members: {props.members}</p>     
-      <input type="text" onChange={props.onChangeName} value={props.name} />
+      <input 
+        className={inputClasses.join(' ')}
+        type="text" 
+        onChange={props.onChangeName} 
+        value={props.name} 
+      />
       <button onClick={props.onDelete}>Delete</button>
     </div>
   )        
